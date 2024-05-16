93ef5b03405b5b21fc3b521ed3446ec27ead0c92
aaynong <aaynongprogram@gmail.com>
2024-05-17 04:45:24 +0700

    again

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -2,7 +2,7 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 export const authOptions = {
   providers: [
-    GithubProvider({
+    GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
