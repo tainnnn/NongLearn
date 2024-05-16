beeb9ac3ae78c47de90a28a57b32c2fc3993c8b9
aaynong <aaynongprogram@gmail.com>
2024-05-17 04:42:35 +0700

    newtwo

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,15 +1,11 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
-
-const authOptions = {
-    providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_ID,
-        clientSecret: process.env.GOOGLE_SECRET,
-      }),
-      
-    ],
-  }
-
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+}
+export default NextAuth(authOptions)
