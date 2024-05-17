512476003591cc74b6dc9b7e9e0a8e75ca55b7fa
aaynong <aaynongprogram@gmail.com>
2024-05-17 19:47:17 +0700

    toshow

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,11 @@
-import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
 
-const handler = NextAuth({
+export default NextAuth({
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID ?? "",
-      clientSecret: process.env.GOOGLE_SECRET ?? "",
+    Providers.Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
 });
-
-export { handler as GET, handler as POST };
