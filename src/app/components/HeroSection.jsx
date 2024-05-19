630af18519f384b6e44ce2bdd43ba1e2def68e48
aaynong <aaynongprogram@gmail.com>
2024-05-19 13:25:04 +0700

    text

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -9,8 +9,8 @@ function HeroSection() {
   return (
     <section className='grid grid-cols-1 lg:grid-cols-12 my-4'>
         <div className='col-span-7 place-self-center place-items-center grid lg:place-items-start'>
-            <h1 className="text-white max-w-2xl mb-4 lg:text-6xl text-4xl font-extrabold">
-                <span className="bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
+            <h1 className="text-white max-w-2xl mb-4 lg:text-8xl text-4xl font-extrabold">
+            <span className="bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block @media (min-width: 768px) { text-transparent bg-clip-text }">
                     Hello{" "}
                 </span>{" "}
           <br />
